9013a5682ea0dd90f40d0390a881223e324df2ec
runnabro <tli415@gmail.com>
2018-08-08 21:20:16 -0700

    fix breadcrumbs
    * add icons home, github, chevron
    * configure stylelint rules and fix lint errors

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('preview', ['build:preview'], () =>
 
 gulp.task('pack', ['build', 'lint'], () => pack(destDir, buildDir, bundleName))
 
-gulp.task('release', ['pack'], () => release(buildDir, bundleName, 'couchbase', 'docs-ui', process.env.GITHUB_TOKEN))
+gulp.task('release', ['pack'], () => release(buildDir, bundleName, 'mulesoft', 'docs-ui', process.env.GITHUB_TOKEN))
 
 gulp.task('default', ['build'])
 

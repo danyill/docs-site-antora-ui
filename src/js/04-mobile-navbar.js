619dca1b04b0483e9cb4117a3242bcc2605e7713
Tony Li <tli415@gmail.com>
2018-08-23 15:08:02 -0700

    add responsive nav

--- src/js/04-mobile-navbar.js
+++ src/js/04-mobile-navbar.js
@@ -1,10 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var navbarToggle = document.querySelector('.navbar-burger')
-  if (!navbarToggle) return
-  navbarToggle.addEventListener('click', function (e) {
-    e.stopPropagation()
-    navbarToggle.classList.toggle('is-active')
-    document.getElementById(navbarToggle.dataset.target).classList.toggle('is-active')
-    document.documentElement.classList.toggle('is-clipped--navbar')
-  })
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.body
+  const navToggle = body.querySelectorAll('.js-nav-toggle')
+  const nav = body.querySelector('.js-nav')
+
+  const openNav = (e) => {
+    clickThru(e)
+    nav.classList.add('active')
+    body.classList.add('no-scroll')
+  }
+
+  const closeNav = () => {
+    nav.classList.remove('active')
+    body.classList.remove('no-scroll')
+  }
+
+  const clickThru = (e) => e.stopPropagation()
+
+  // navtoggle listeners
+  for (let i = 0; i < navToggle.length; i++) {
+    navToggle[i].addEventListener('click', openNav)
+    navToggle[i].addEventListener('touchend', openNav)
+  }
+
+  // body listener
+  body.addEventListener('click', closeNav)
+  body.addEventListener('touchend', closeNav)
+
+  // prevent clicks on nav from closing
+  nav.addEventListener('click', clickThru)
+  nav.addEventListener('touchend', clickThru)
 })

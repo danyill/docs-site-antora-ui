a1eebccceb25cf556f968cf00423ce2c5a52b415
Tony Li <tli415@gmail.com>
2018-08-29 17:02:48 -0700

    add source filter

--- src/js/07-coveo.js
+++ src/js/07-coveo.js
@@ -3,21 +3,44 @@
 
   document.addEventListener('DOMContentLoaded', () => {
     // coveo setup
-    const root = document.querySelector('.js-coveo')
+    const body = document.body
+    const root = body.querySelector('.js-coveo')
     Coveo.SearchEndpoint.endpoints['default'] = new Coveo.SearchEndpoint({
       restUri: 'https://platform.cloud.coveo.com/rest/search',
       accessToken: 'xxfb311f5b-4798-4ee9-b059-bd0cbc971104',
     })
+    root.addEventListener('buildingQuery', (e) => {
+      const facetElement = document.querySelector('.CoveoFacet[data-title="Source"]')
+      const facetInstance = Coveo.get(facetElement)
+      facetInstance.selectValue('mulesoft docs staging')
+    })
+
+    // modal setup
+    const backdrop = document.querySelector('.modal-backdrop')
 
     // show/hide coveo search
-    const searchTrigger = document.querySelector('.js-search-trigger')
-    const searchUI = document.querySelector('.js-search-ui')
+    const searchTrigger = body.querySelector('.js-search-trigger')
+    const searchUI = body.querySelector('.js-search-ui')
     const showCoveo = () => {
-      searchUI.classList.toggle('show')
+      backdrop.classList.add('show')
+      body.classList.add('no-scroll')
+      searchUI.classList.add('show')
     }
+    const hideCoveo = () => {
+      backdrop.classList.remove('show')
+      body.classList.add('no-scroll')
+      searchUI.classList.remove('show')
+    }
+    const clickThru = (e) => e.stopPropagation()
 
     searchTrigger.addEventListener('click', showCoveo)
     searchTrigger.addEventListener('touchend', showCoveo)
+    body.addEventListener('click', hideCoveo)
+    body.addEventListener('touchend', hideCoveo)
+
+    // prevent clicks on nav from closing
+    root.addEventListener('click', clickThru)
+    root.addEventListener('touchend', clickThru)
 
     // init
     Coveo.init(root)

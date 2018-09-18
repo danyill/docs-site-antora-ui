a93338a595aebf51fdc5a65fa12f36a5ac12011d
Tony Li <tli415@gmail.com>
2018-09-18 16:45:43 -0700

    test nav fix

--- src/js/05-nav-nested.js
+++ src/js/05-nav-nested.js
@@ -6,14 +6,15 @@
 
     // find and expand list if there's a nested active link
     for (let i = 0; i < nestedNavs.length; i++) {
-      let nestedLinks = nestedNavs[i].parentNode.querySelectorAll('ol a')
+      let nestedLinks = nestedNavs[i].nextElementSibling.querySelectorAll('.nav-link')
+
       for (let i = 0; i < nestedLinks.length; i++) {
         if (nestedLinks[i].classList.contains('active')) {
           let thisList = nestedLinks[i]
           while (!thisList.classList.contains('nav-list')) {
             thisList = thisList.parentNode
           }
-          thisList.parentNode.querySelector('.js-nav-nested').classList.add('expanded')
+          thisList.previousElementSibling.classList.add('expanded')
         }
       }
     }

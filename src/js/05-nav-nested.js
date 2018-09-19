9b41c8b77aee814273ac2e1e9be5bc2a6ff8dae5
Tony Li <tli415@gmail.com>
2018-09-18 17:11:47 -0700

    expand 2 more levels

--- src/js/05-nav-nested.js
+++ src/js/05-nav-nested.js
@@ -10,11 +10,24 @@
 
       for (let i = 0; i < nestedLinks.length; i++) {
         if (nestedLinks[i].classList.contains('active')) {
+          // get this active list & expand it
           let thisList = nestedLinks[i]
           while (!thisList.classList.contains('nav-list')) {
             thisList = thisList.parentNode
           }
           thisList.previousElementSibling.classList.add('expanded')
+
+          // check if this is a nested, nested list
+          const thisListParent = thisList.parentNode.parentNode
+          if (!thisListParent.classList.contains('parent')) {
+            thisListParent.previousElementSibling.classList.add('expanded')
+          }
+
+          // check if this is a nested, nested, nested list
+          const thisListParentParent = thisListParent.parentNode.parentNode
+          if (!thisListParentParent.classList.contains('parent')) {
+            thisListParentParent.previousElementSibling.classList.add('expanded')
+          }
         }
       }
     }

4a6c30b8433514a98306e6171dedb351dac6f0a3
Tony Li <tli415@gmail.com>
2018-09-19 18:04:14 -0700

    say thanks when giving feedback

--- src/js/06-jira-issues.js
+++ src/js/06-jira-issues.js
@@ -1,10 +1,25 @@
 ;(() => {
+  // open jira dialog
   window.ATL_JQ_PAGE_PROPS = {
     'triggerFunction': (showCollectorDialog) => {
       document.querySelector('.js-jira').addEventListener('click', (e) => {
         e.preventDefault()
         showCollectorDialog()
       })
+      document.querySelector('.js-jira').addEventListener('touchend', (e) => {
+        e.preventDefault()
+        showCollectorDialog()
+      })
     },
   }
+
+  // saying thanks
+  const thanksSection = document.querySelector('.js-thanks-section')
+  const thanksTrigger = thanksSection.querySelector('.js-thanks')
+  const sayThanks = () => thanksSection.classList.add('flip')
+
+  document.addEventListener('DOMContentLoaded', () => {
+    thanksTrigger.addEventListener('click', sayThanks)
+    thanksTrigger.addEventListener('touchend', sayThanks)
+  })
 })()

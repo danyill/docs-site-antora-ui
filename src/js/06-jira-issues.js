698bb3d05531626255432b6733a9436cf76d7f9b
Tony Li <tli415@gmail.com>
2018-09-19 18:25:00 -0700

    add tracking events for thanks and github

--- src/js/06-jira-issues.js
+++ src/js/06-jira-issues.js
@@ -1,14 +1,23 @@
 ;(() => {
+  // track not helpful
+  const trackNotHelpful = () => {
+    analytics.track('Clicked Helpful No', {
+      url: window.location.href,
+    })
+  }
+
   // open jira dialog
   window.ATL_JQ_PAGE_PROPS = {
     'triggerFunction': (showCollectorDialog) => {
       document.querySelector('.js-jira').addEventListener('click', (e) => {
         e.preventDefault()
         showCollectorDialog()
+        trackNotHelpful()
       })
       document.querySelector('.js-jira').addEventListener('touchend', (e) => {
         e.preventDefault()
         showCollectorDialog()
+        trackNotHelpful()
       })
     },
   }
@@ -16,7 +25,12 @@
   // saying thanks
   const thanksSection = document.querySelector('.js-thanks-section')
   const thanksTrigger = thanksSection.querySelector('.js-thanks')
-  const sayThanks = () => thanksSection.classList.add('flip')
+  const sayThanks = () => {
+    thanksSection.classList.add('flip')
+    analytics.track('Clicked Helpful Yes', {
+      url: window.location.href,
+    })
+  }
 
   document.addEventListener('DOMContentLoaded', () => {
     thanksTrigger.addEventListener('click', sayThanks)

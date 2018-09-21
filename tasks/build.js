cc40e7d03c204231d7f37e079f14c6bb11a144a6
Tony Li <tli415@gmail.com>
2018-09-21 11:03:17 -0700

    add babel

--- tasks/build.js
+++ tasks/build.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const babel = require('gulp-babel')
 const browserify = require('browserify')
 const buffer = require('vinyl-buffer')
 const concat = require('gulp-concat')
@@ -57,6 +58,7 @@ module.exports = (src, dest, preview) => {
   return merge([
     vfs
       .src('js/+([0-9])-*.js', opts)
+      // .pipe(babel())
       // .pipe(uglify())
       .pipe(concat('js/site.js')),
 
